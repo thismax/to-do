593c135c26234f913554268c7f41760f8688cd9a
thismax <max.g.jacobs@gmail.com>
2017-09-14 16:39:49 -0700

    set up server

--- server/server.js
+++ server/server.js
@@ -1,4 +1,9 @@
-var express = require('express');
-var parse = require('body-parser');
-var path = require('path');
-
+const express = require('express');
+const parser = require('body-parser');
+const path = require('path');
+const router = require('express').Router();
+const app = express();
+app.use(parser.json());
+app.use('/', express.static(path.resolve(__dirname, '../public')));
+app.get('/')
+app.listen(3000, ()=> console.log('listening on port 3000'));
