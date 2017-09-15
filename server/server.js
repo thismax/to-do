d99e41952052c1c7b34d0cd8ad558a753114ffcc
thismax <max.g.jacobs@gmail.com>
2017-09-14 22:29:18 -0700

    added more middleware

--- server/server.js
+++ server/server.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const path = require('path');
-const router = require('express').Router();
 const db = require('./db');
+const routes = require('./routes');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 //instatiate express server
 const app = express();
-//server client files
+//middleware
+app.use(bodyParser.json());
 app.use('/', express.static(path.resolve(__dirname, '../public')));
+app.use('/', routes);
+app.use(cors())
 //sync db before listening
 if (!module.parent) {
-  db.User.sync().then(() => {
-    db.Todo.sync().then(() => {
+  db.User.sync({force: true}).then(() => {
+    db.Todo.sync({force: true}).then(() => {
       app.listen(3000, () => console.log('listening on port 3000'));
     });
   });
