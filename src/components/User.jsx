b7e616b7ad00bd08cabea6d60098ae1e487e6657
thismax <max.g.jacobs@gmail.com>
2017-09-15 14:30:30 -0700

    init

---
+++ src/components/User.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+const User = ({user}) => {
+    return (
+      <li class="user">{user}</li>
+    );
+}
+
+export default User;
---
+++ src/components/Users.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+const Users = ({users}) => {
+    return (
+      <ul class="users">{users}</ul>
+    );
+}
+
+export default Users;
