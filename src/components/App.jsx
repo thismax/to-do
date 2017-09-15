0876fb1ef9d86767600b0b5189f6b5001b6c1579
thismax <max.g.jacobs@gmail.com>
2017-09-15 12:28:03 -0700

    init

---
+++ src/components/App.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+  }
+
+  componentDidMount() {
+
+  }
+
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
+
+export default App;
