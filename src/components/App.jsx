3b0fef87a77204de065f2bb774ab21ef1b3bb572
thismax <max.g.jacobs@gmail.com>
2017-09-15 17:59:24 -0700

    added dummy data

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,37 @@
 import React, { Component } from 'react';
-import axios from 'axios'
+import axios from 'axios';
 import Todos from './Todos';
 import Users from './Users';
 
 class App extends Component {
   constructor(props) {
     super(props);
+    this.state ={
+      todos: []
+    }
+  }
 
+  componentDidMount() {
+    this.setState({
+      todos: [
+        {
+          "username": "The Rod",
+          "todo": "kick ass",
+          "completed": false
+        },
+        {
+          "username": "The Rod",
+          "todo": "chew bubblegum",
+          "completed": true
+        }
+      ]
+    });
   }
 
   render() {
     return (
       <div>
-        {/* <Users /> */}
-        <Todos />
+       <Todos todos={this.state.todos}/>
       </div>
     )
   }
