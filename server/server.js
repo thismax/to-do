b47dd33dae130201e18dc37b168db73a2d02797c
thismax <max.g.jacobs@gmail.com>
2017-09-15 19:53:24 -0700

    needed to relocate cors

--- server/server.js
+++ server/server.js
@@ -7,15 +7,17 @@ const cors = require('cors');
 //instatiate express server
 const app = express();
 //middleware
+app.use(cors());
 app.use(bodyParser.json());
 app.use('/', express.static(path.resolve(__dirname, '../static')));
 app.use('/', routes);
-app.use(cors())
 //sync db before listening
 if (!module.parent) {
-  db.User.sync({force: true}).then(() => {
-    db.Todo.sync({force: true}).then(() => {
+  db.User.sync().then(() => {
+    db.Todo.sync().then(() => {
       app.listen(3000, () => console.log('listening on port 3000'));
     });
   });
 }
+
+//force: true is a good optoin for sync - it wipes the tables
