5ead6dab5c778cefdc54df83d2c053da8f00ba3d
thismax <max.g.jacobs@gmail.com>
2017-09-16 11:08:12 -0700

    added routes

--- server/routes.js
+++ server/routes.js
@@ -3,5 +3,7 @@ const controller = require('./controller');
 
 router.post('/todos', controller.todos.post);
 router.get('/todos', controller.todos.get);
+router.put('/todos', controller.todos.put);
+router.delete('/todos', controller.todos.delete);
 
 module.exports = router;
